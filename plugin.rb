4ace47b9388c1df35cdcaff74bcb6f51d6d8b2a8
KAJATIGER <ksantro@gmx.de>
2017-07-31 10:47:31 +0200

    had to change naming in the yml files too. Added a list of subclass plugins to the Readme

--- plugin.rb
+++ plugin.rb
@@ -1,12 +1,12 @@
-# name: discourse-basic-sync-plugin
+# name: discourse-sync-base
 # about: -
 # version: 1.0
 # authors: Kaja & Jen
-# url: https://github.com/berlindiamonds/basic-sync-plugin
+# url: https://github.com/berlindiamonds/discourse-sync-base
 
 require 'sidekiq'
 
-enabled_site_setting :discourse_basic_sync_plugin_enabled
+enabled_site_setting :discourse_sync_base_enabled
 
 after_initialize do
   load File.expand_path("../app/jobs/regular/sync_backups.rb", __FILE__)

6418cf48068370c8ef35fbd7ab84ffec5980597d
Jen <jvarelalijo@gmail.com>
2017-08-01 17:13:26 +0200

    delete extra files and code

--- plugin.rb
+++ plugin.rb
@@ -1,20 +1,11 @@
-# name: discourse-basic-sync-plugin
+# name: discourse-sync-base
 # about: -
 # version: 1.0
 # authors: Kaja & Jen
 # url: https://github.com/berlindiamonds/basic-sync-plugin
 
-require 'sidekiq'
-
-enabled_site_setting :discourse_basic_sync_plugin_enabled
+enabled_site_setting :discourse_sync_base_enabled
 
 after_initialize do
-  load File.expand_path("../app/jobs/regular/sync_backups.rb", __FILE__)
   load File.expand_path("../lib/synchronizer.rb", __FILE__)
-
-  Backup.class_eval do
-    def after_create_hook
-      Jobs.enqueue(:sync_backups)
-    end
-  end
 end
